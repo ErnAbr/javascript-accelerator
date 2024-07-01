d8116fa991b823c71889ad0693d7f4fb0265bafd
ErnAbr <ern_abr@yahoo.com>
2024-07-01 20:35:28 +0300

    useLocalStorage hook and selecting favourite services

--- Home Service Booking App/src/components/Card/Card.jsx
+++ Home Service Booking App/src/components/Card/Card.jsx
@@ -1,26 +1,41 @@
 import PropTypes from "prop-types";
 import { Button } from "../Button/Button";
 import styles from "./Card.module.scss";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
-export const Card = ({ image, serviceCategory, serviceType, servicePersonName, serviceAddress, cardId }) => {
-  console.log(cardId);
+export const Card = ({
+  image,
+  serviceCategory,
+  serviceType,
+  servicePersonName,
+  serviceAddress,
+  isFavorite,
+  toggleFavorite,
+}) => {
   return (
-    <>
-      <div className={styles.card}>
-        <img className={styles.imgStyle} src={image} alt="card-image" />
-        <div className={styles.cardBody}>
-          <div>
-            <div className={styles.serviceCategory}>{serviceCategory}</div>
-          </div>
-          <h4>{serviceType}</h4>
-          <div>
-            <div className={styles.servicePersonName}>{servicePersonName}</div>
-            <p>{serviceAddress}</p>
-          </div>
+    <div className={styles.card}>
+      <img className={styles.imgStyle} src={image} alt="card-image" />
+      <div className={styles.cardBody}>
+        <div>
+          <div className={styles.serviceCategory}>{serviceCategory}</div>
+        </div>
+        <h4>{serviceType}</h4>
+        <div>
+          <div className={styles.servicePersonName}>{servicePersonName}</div>
+          <p>{serviceAddress}</p>
+        </div>
+        <div className={styles.cardBottom}>
           <Button buttonType="primary">Book Now</Button>
+          <div onClick={toggleFavorite}>
+            {isFavorite ? (
+              <FaHeart size="2em" className={styles.favCard} />
+            ) : (
+              <FaRegHeart size="2em" className={styles.heartIcon} />
+            )}
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
@@ -31,4 +46,6 @@ Card.propTypes = {
   serviceType: PropTypes.string.isRequired,
   servicePersonName: PropTypes.string.isRequired,
   serviceAddress: PropTypes.string.isRequired,
+  isFavorite: PropTypes.bool.isRequired,
+  toggleFavorite: PropTypes.func.isRequired,
 };
